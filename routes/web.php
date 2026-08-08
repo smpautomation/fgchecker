3aa82c45b8c1711d9cb4b67b3a89a52929cb09fe
bellosillo137847 <jbellosillo.137847@gmail.com>
2026-08-08 17:36:40 +0800

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\FGCheckerRecordController;
-use App\Http\Controllers\FgCheckerScanController;
+use App\Http\Controllers\FGCheckerScanController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
